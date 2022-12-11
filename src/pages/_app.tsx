bcaeef97a8364651df3d219f6d1bea4e6105e06c
William Silva <euwilliamsantana@gmail.com>
2022-12-11 07:45:12 -0300

    Estrutura visual - Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,16 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      
+
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
      <Component {...pageProps} />
