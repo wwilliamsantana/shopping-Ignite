b023a1521573b1ba8ba37042240c788f90dfe9fb
William Silva <euwilliamsantana@gmail.com>
2022-12-11 08:19:13 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-
-const Button = styled("button", {
-
-  backgroundColor: "$green500",
-  padding: "10px 12px",
-  borderRadius: 6,
-  color: "White",
-  alignSelf: "flex-start",
-
-  "&:hover":{
-    filter: "brightness(0.8)"
-  }
-
-})
+import camiseta1 from "../assets/Shirt/1.png"
+import camiseta2 from "../assets/Shirt/2.png"
+import camiseta3 from "../assets/Shirt/3.png"
 
 
 export default function Home() {
   return (
-    <Button>
-      Hello World
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      
+    </HomeContainer>
   )
 }
