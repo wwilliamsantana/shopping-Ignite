8a5338cadaecf5ae6021112b2ae4acbb57b66b55
William Silva <euwilliamsantana@gmail.com>
2022-12-12 11:30:54 -0300

    Estrutura- Remove Arrow Carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,14 @@ import { HomeContainer, Product } from "../styles/pages/home"
 import { useKeenSlider} from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 
-import { useState } from "react"
-import { Arrow } from "../components/Arrow"
 import camiseta1 from "../assets/Shirt/1.png"
 
-
-
 export default function Home() {
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const [loaded, setLoaded] = useState(false)
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
     slides:{
       perView: 3,
       spacing: 48
-    },
-    initial: 0,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel)
-    },
-    created() {
-      setLoaded(true)
-    },
+    }
   })
 
   return (
@@ -61,30 +48,6 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-
-      <div> 
-        {loaded && instanceRef.current && (
-            <>
-              <Arrow
-                left
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.prev()
-                }
-                disabled={currentSlide === 0}
-              />
-
-              <Arrow
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.next()
-                }
-                disabled={
-                  currentSlide ===
-                  instanceRef.current.track.details.slides.length - 1
-                }
-              />
-            </>
-          )}
-      </div>
 
     </HomeContainer>
   )
