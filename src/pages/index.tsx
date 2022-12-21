80e67dc50fe11110f0695a858336428b7890bc2e
William Silva <euwilliamsantana@gmail.com>
2022-12-21 11:18:22 -0300

    Produto & Checkout - Prefetch de links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({products}: HomeProps) {
 
      {products.map(product => {
       return (
-        <Link key={product.id}  href={`/product/${product.id}`}>
+        <Link key={product.id}  href={`/product/${product.id}`} prefetch={false}>
           <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt=""/>
             <footer>
