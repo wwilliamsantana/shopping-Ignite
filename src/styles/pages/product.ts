b3dc2b05fa4c4aa5ac6acc631a5ac118184741f3
William Silva <euwilliamsantana@gmail.com>
2022-12-24 18:09:42 -0300

    Styles bug: OK

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -1,76 +1,73 @@
 import { styled } from "..";
 
-
-export const ProductContainer = styled("main", {
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr",
-  alignItems: "stretch",
-  gap: "4rem",
+export const ProductContainer = styled('main', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  alignItems: 'stretch',
+  gap: '4rem',
 
   maxWidth: 1180,
-  margin: "0 auto"
+  margin: '0 auto'
 })
 
-export const ImageContainer = styled("div", {
-  width: "100%",
+export const ImageContainer = styled('div', {
+  width: '100%',
   maxWidth: 576,
   height: 656,
-  background: "linear-gradient(100deg, #1ea483 0%, #7465d4 100%)",
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: "0.25rem",
+  padding: '0.25rem',
 
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 
-  img:{
-    objectFit: "cover"
+  img: {
+    objectFit: 'cover'
   }
-
 })
 
-export const ProductDetails = styled("div", {
-  display: "flex",
-  flexDirection: "column",
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
 
-  h1:{
-    fontSize: "$2xl",
-    color: "$gray300"
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
   },
 
-  span:{
-    marginTop: "1rem",
-    display: "block",
-    fontSize: "$2xl",
-    color: "$green300"
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300',
   },
 
   p: {
-    marginTop: "2.5rem",
-    fontSize: "$md",
+    marginTop: '2.5rem',
+    fontSize: '$md',
     lineHeight: 1.6,
-    color: "$gray300"
+    color: '$gray300',
   },
 
   button: {
-    marginTop: "auto",
-    background: "$green500",
-    color: "$white",
+    marginTop: 'auto',
+    backgroundColor: '$green500',
     border: 0,
-    fontSize: "$md",
+    color: '$white',
     borderRadius: 8,
-    padding: "1.25rem",
-    fontWeight: "bold",
-    cursor: "pointer",
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
 
-    "&:disabled": {
-      cursor: "not-allowed",
+    '&:disabled': {
       opacity: 0.6,
+      cursor: 'not-allowed',
     },
 
-    "&:not(:disabled):hover":{
-      background: "$green300",
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300',
     }
-  }
-
+  },
 })
